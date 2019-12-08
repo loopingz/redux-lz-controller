50bd61df8ca1803d3c5e196492dc53b0ac9e2632
Remi Cattiau <remi@cattiau.com>
2019-12-08 09:41:38 -0800

    ci: add more asserts

--- src/controller.spec.ts
+++ src/controller.spec.ts
@@ -5,9 +5,19 @@ import * as Redux from "redux";
 import thunk from "redux-thunk";
 import { Controller } from "./controller";
 
+function promiseState(p) {
+  const t = {};
+  return Promise.race([p, t]).then(
+    v => (v === t ? "pending" : "fulfilled"),
+    () => "rejected"
+  );
+}
+
 class TestController extends Controller {
+  onAsyncSuccess: boolean = false;
+  onAsyncFailed: boolean = false;
   constructor() {
-    super("test", {});
+    super("test", { someInfos: true });
   }
 
   init() {
@@ -23,53 +33,65 @@ class TestController extends Controller {
     return this.getState();
   }
 
-  launchActionEmpty() {
-    this.asyncAction(
-      "TEST_ASYNC",
-      async () => {
-        await this.ajax("/TestEmpty", "GET");
-      },
-      () => {}
-    );
+  async launchActionEmpty() {
+    return new Promise(resolve => {
+      this.asyncAction(
+        "TEST_ASYNC",
+        async () => {
+          await this.ajax("/TestEmpty", "GET");
+        },
+        resolve
+      );
+    });
   }
 
-  launchActionError() {
-    this.asyncAction(
-      "TEST_ASYNC",
-      async () => {
+  async launchActionError() {
+    return new Promise(resolve => {
+      this.asyncAction("TEST_ASYNC", async () => {
         await this.ajax("/TestError", "GET");
-      },
-      () => {}
-    );
+      });
+      // Use a 10ms delay as we want to test error
+      setTimeout(resolve, 10);
+    });
   }
 
-  launchActionRedirect() {
-    this.asyncAction(
-      "TEST_ASYNC",
-      async () => {
+  async launchActionRedirect() {
+    return new Promise(resolve => {
+      this.asyncAction("TEST_ASYNC", async () => {
         await this.ajax("/TestRedirect", "GET");
-      },
-      () => {}
-    );
+      });
+      // Use a 10ms delay as we want to test error
+      setTimeout(resolve, 10);
+    });
   }
 
-  launchActionOk() {
-    this.asyncAction(
-      "TEST_ASYNC",
-      async () => {
-        await this.ajax("/TestOk", "GET");
-      },
-      () => {}
-    );
+  async launchActionOk() {
+    return new Promise(resolve => {
+      this.asyncAction(
+        "TEST_ASYNC",
+        async () => {
+          await this.ajax("/TestOk", "GET");
+        },
+        resolve
+      );
+    });
   }
 
   onTEST_ACTION() {
     this.setInitialized();
   }
 
-  onTEST_ASYNC_SUCCESS() {}
+  onTEST_ASYNC_SUCCESS() {
+    this.onAsyncSuccess = true;
+  }
 
-  onTEST_ASYNC_FAILED() {}
+  onTEST_ASYNC_FAILED() {
+    this.onAsyncFailed = true;
+  }
+
+  reset() {
+    this.onAsyncSuccess = this.onAsyncFailed = false;
+  }
 }
 let testController = new TestController();
 
@@ -86,15 +108,21 @@ class Test2Controller extends Controller {
     Controller.dispatch({ type: "TEST_ACTION" });
   }
 
-  launchAsyncAction() {
-    this.asyncAction("TEST_ASYNC", async () => {
-      await this.ajax("/TestOk");
+  async launchAsyncAction() {
+    return new Promise(resolve => {
+      this.asyncAction("TEST_ASYNC", async () => {
+        await this.ajax("/TestOk");
+      });
+      setTimeout(resolve, 10);
     });
   }
 
-  launchAsyncActionFailure() {
-    this.asyncAction("TEST_ASYNC", async () => {
-      await this.ajax("/TestError");
+  async launchAsyncActionFailure() {
+    return new Promise(resolve => {
+      this.asyncAction("TEST_ASYNC", async () => {
+        await this.ajax("/TestError");
+      });
+      setTimeout(resolve, 10);
     });
   }
 
@@ -122,21 +150,35 @@ class ReduxControllerTest {
   @test
   async ajax() {
     Controller.setEndpoint("http://localhost:18080");
-    testController.launchActionEmpty();
-    testController.launchActionError();
-    testController.launchActionOk();
-    testController.launchActionRedirect();
+    await testController.launchActionEmpty();
+    assert.equal(testController.onAsyncFailed, false);
+    assert.equal(testController.onAsyncSuccess, true);
+    testController.reset();
+    await testController.launchActionError();
+    assert.equal(testController.onAsyncFailed, true);
+    assert.equal(testController.onAsyncSuccess, false);
+    testController.reset();
+    await testController.launchActionOk();
+    assert.equal(testController.onAsyncFailed, false);
+    assert.equal(testController.onAsyncSuccess, true);
+    testController.reset();
+    await testController.launchActionRedirect();
+    assert.equal(testController.onAsyncFailed, false);
+    assert.equal(testController.onAsyncSuccess, true);
+    testController.reset();
     let promise = testController.waitInit();
-    // check prommise is pending
+    assert.equal(await promiseState(promise), "pending");
+    // Will launch the onTEST_ACTION that will finish the init of testController with afterTEST_ACTION
     test2Controller.launchAction();
     await promise;
     await testController.waitInit();
+    assert.equal(await promiseState(promise), "fulfilled");
   }
 
   @test
-  undefinedReducer() {
-    test2Controller.launchAsyncAction();
-    test2Controller.launchAsyncActionFailure();
+  async undefinedReducer() {
+    await test2Controller.launchAsyncAction();
+    await test2Controller.launchAsyncActionFailure();
   }
 
   @test
@@ -149,6 +191,8 @@ class ReduxControllerTest {
   @test
   getState() {
     assert.notEqual(testController.localState(), undefined);
+    assert.equal(testController.localState().someInfos, true);
     assert.notEqual(testController.state(), undefined);
+    assert.equal(testController.state().test.someInfos, true);
   }
 }
